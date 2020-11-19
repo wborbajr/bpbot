620fd45d4aa29a6545cc0251c65eb2fc72022732
Waldir Borba Junior <wborbajr@gmail.com>
2020-11-19 14:14:16 -0300

    hore: removed log support due to permission conflict

--- main.go
+++ main.go
@@ -22,19 +22,6 @@ func init() {
 }
 
 func main() {
-
-	// Configure Logging
-	// LOG_FILE_LOCATION := os.Getenv("LOG_FILE_LOCATION")
-	// if LOG_FILE_LOCATION != "" {
-	// 	log.SetOutput(&lumberjack.Logger{
-	// 		Filename:   LOG_FILE_LOCATION,
-	// 		MaxSize:    500, // megabytes
-	// 		MaxBackups: 3,
-	// 		MaxAge:     28,   //days
-	// 		Compress:   true, // disabled by default
-	// 	})
-	// }
-
 	log.Println("Starting... "+ system_name + " " + system_version)
 
 	server.SetupApp()
