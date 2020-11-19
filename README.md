0ce14173e0df1baf5537b7cf64470b87c5ce0044
Waldir Borba Junior <wborbajr@gmail.com>
2020-11-19 12:44:30 -0300

    chore: code refactored

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 B+ Bot
 
 #### Tasks
+
 ```bash
 docker build --pull --rm -f Dockerfile -t wborbajr/bpbot:latest "."
 
@@ -13,3 +14,23 @@ docker run -p 3001:3443 -it bpbot:latest /bin/bash
 
 docker run -p 3001:3443 -it bpbot:latest
 ```
+
+#### WatchTower
+
+```bash
+docker run -d  --restart always \
+  --name watchtower \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  containrrr/watchtower --debug --interval 600
+  ```
+
+  #### Raspi
+
+  Fixing docker permission error
+
+  ```bash
+  sudo groupadd docker
+  systemctl restart docker
+  gpasswd -a $USER docker
+  sudo usermod -a -G docker $USER
+  ```
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"log"
+
 	"github.com/joho/godotenv"
 
 	"github.com/wborbajr/bpbot/server"
-	bplogger "github.com/wborbajr/bpbot/utils"
 )
 
 const (
@@ -22,7 +23,19 @@ func init() {
 
 func main() {
 
-	bplogger.GeneralLogger.Println("Starting... "+ system_name + " " + system_version)
+	// Configure Logging
+	// LOG_FILE_LOCATION := os.Getenv("LOG_FILE_LOCATION")
+	// if LOG_FILE_LOCATION != "" {
+	// 	log.SetOutput(&lumberjack.Logger{
+	// 		Filename:   LOG_FILE_LOCATION,
+	// 		MaxSize:    500, // megabytes
+	// 		MaxBackups: 3,
+	// 		MaxAge:     28,   //days
+	// 		Compress:   true, // disabled by default
+	// 	})
+	// }
+
+	log.Println("Starting... "+ system_name + " " + system_version)
 
 	server.SetupApp()
 
